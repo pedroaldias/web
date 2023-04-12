a79008685b63b3d2349cdf2fce3fc1ecab72929f
pedroaldias <pedroaffonsoldias@gmail.com>
2023-04-12 13:12:11 -0300

    almost finished

--- ex12/js/index.js
+++ ex12/js/index.js
@@ -1,207 +1,203 @@
 
-function getCategoriaSelecionada() {
-    var value = categoria.options[categoria.selectedIndex].value;
-    return value;
-}
+// function getCategoriaSelecionada() {
+//     var value = categoria.options[categoria.selectedIndex].value;
+//     return value;
+// }
 
-function getMes(data){
-    return new Date(data).getMonth() + 1
-}
+// function getMes(data){
+//     return new Date(data).getMonth() + 1
+// }
 
-function getAno(data){
-    return new Date(data).getFullYear();
-}
+// function getAno(data){
+//     return new Date(data).getFullYear();
+// }
 
-function verificarValor() {
-    valorErro.innerHTML = ''
-    if (parseInt(valor.value) < 0) {
-        valorErro.style.color = 'red';
-        valorErro.innerHTML = 'Valor deve ser maior ou igual a 0';
-    }
-}
+// function verificarValor() {
+//     valorErro.innerHTML = ''
+//     if (parseFloat(valor.value) < 0) {
+//         valorErro.style.color = 'red';
+//         valorErro.innerHTML = 'Valor deve ser maior ou igual a 0';
+//     }
+// }
 
-function adicionar() {
-    //Criando uma nova linha e novas células para categoria, data e valor
-    let novaLinha = extrato.insertRow(-1);
-    let celulaCategoria = novaLinha.insertCell(0);
-    let celulaData = novaLinha.insertCell(1);
-    let celulaValor = novaLinha.insertCell(2);
+// function adicionar() {
+//     //Criando uma nova linha e novas células para categoria, data e valor
+//     let novaLinha = extrato.insertRow(-1);
+//     let celulaCategoria = novaLinha.insertCell(0);
+//     let celulaData = novaLinha.insertCell(1);
+//     let celulaValor = novaLinha.insertCell(2);
     
 
-    // Atribuindo valor para a célula de categoria
-    switch (categoria.value) {  
-        case 'mercado':
-            var categoriaNova = 'Mercado';
-            break;
-        case 'transporte':
-            var categoriaNova = 'Transporte';
-            break;
-    }
-    let novoDadoCategoria = document.createTextNode(categoriaNova);
-    celulaCategoria.appendChild(novoDadoCategoria);
+//     // Atribuindo valor para a célula de categoria
+//     switch (categoria.value) {  
+//         case 'mercado':
+//             var categoriaNova = 'Mercado';
+//             break;
+//         case 'transporte':
+//             var categoriaNova = 'Transporte';
+//             break;
+//     }
+//     let novoDadoCategoria = document.createTextNode(categoriaNova);
+//     celulaCategoria.appendChild(novoDadoCategoria);
 
-    // Atribuindo valor para a célula de data
-    let dataNova = data.value;
-    let novoDadoData = document.createTextNode(dataNova);
-    celulaData.appendChild(novoDadoData);
+//     // Atribuindo valor para a célula de data
+//     let dataNova = data.value;
+//     let novoDadoData = document.createTextNode(dataNova);
+//     celulaData.appendChild(novoDadoData);
 
-    // Atribuindo valor para a célula de valor
-    if (valor.value > 0) {
-        var valorNovo = valor.value;
-        let novoDadoValor = document.createTextNode(valorNovo);
-        celulaValor.appendChild(novoDadoValor);
-    }
-    if(getCategoriaSelecionada() == 'transporte'){
-        valorTransporte += parseInt(valorNovo);
-    } else if(getCategoriaSelecionada() == 'mercado'){
-        valorMercado += parseInt(valorNovo);
-    }
+//     // Atribuindo valor para a célula de valor
+//     if (valor.value > 0) {
+//         var valorNovo = valor.value;
+//         let novoDadoValor = document.createTextNode(valorNovo);
+//         celulaValor.appendChild(novoDadoValor);
+//     }
+//     if(getCategoriaSelecionada() == 'transporte'){
+//         valorTransporte += parseFloat(valorNovo);
+//     } else if(getCategoriaSelecionada() == 'mercado'){
+//         valorMercado += parseFloat(valorNovo);
+//     }
 
-    categoria.value = '';
-    valor.value = '';
-    data.value = '';    
-}
+//     categoria.value = '';
+//     valor.value = '';
+//     data.value = '';    
+// }
 
-function calcular() {
-    // Pegando todas as linhas da tabela
-    var linhas = extrato.getElementsByTagName('tr');
+// function calcular() {
+//     // Pegando todas as linhas da tabela
+//     var linhas = extrato.getElementsByTagName('tr');
     
-    let mesesValor = {};
-    let valorTotal = 0;
-    let anos = new Array();
+//     let mesesValor = {};
+//     let valorTotal = 0;
+//     let anos = new Array();
 
-    for (let i = 1; i < linhas.length; i++) {
-        // Pegando o valor da célula de valor e atribuindo à variável de valor total / variável de soma por categoria
-        let valorCelulaValor = parseFloat(linhas[i].getElementsByTagName('td')[2].innerHTML);
-        valorTotal += valorCelulaValor;
+//     for (let i = 1; i < linhas.length; i++) {
+//         // Pegando o valor da célula de valor e atribuindo à variável de valor total / variável de soma por categoria
+//         let valorCelulaValor = parseFloat(linhas[i].getElementsByTagName('td')[2].innerHTML);
+//         valorTotal += valorCelulaValor;
 
 
-        //Pegando o mês da célula de data
-        let valorCelulaData = linhas[i].getElementsByTagName('td')[1].innerHTML;
-        let mes = parseInt(getMes(valorCelulaData))
-        let ano = parseInt(getAno(valorCelulaData))
+//         //Pegando o mês da célula de data
+//         let valorCelulaData = linhas[i].getElementsByTagName('td')[1].innerHTML;
+//         let getMes = parseFloat(getMes(valorCelulaData))
+//         let ano = parseFloat(getAno(valorCelulaData))
         
-        let valor = parseFloat(linhas[i].getElementsByTagName('td')[2].innerHTML);
-        if(mesesValor[mes] !== undefined) {
-            mesesValor[mes] += valor;
-            anos.push(ano)
-        } else {
-            mesesValor[mes] = valor;
-            anos.push(ano)
+//         let valor = parseFloat(linhas[i].getElementsByTagName('td')[2].innerHTML);
+//         if(mesesValor[getMes] !== undefined) {
+//             mesesValor[getMes] += valor;
+//             anos.push(ano)
+//         } else {
+//             mesesValor[getMes] = valor;
+//             anos.push(ano)
             
-        }
-    }
-    localStorage.setItem('arrayAnos', JSON.stringify(anos))
-    localStorage.setItem('mesesValor', JSON.stringify(mesesValor))
-    localStorage.setItem('total', valorTotal);
-    localStorage.setItem('valorMercado', valorMercado);
-    localStorage.setItem('valorTransporte', valorTransporte);
-    window.location.href = 'resumo.html';
-}
+//         }
+//     }
+//     localStorage.setItem('arrayAnos', JSON.stringify(anos))
+//     localStorage.setItem('mesesValor', JSON.stringify(mesesValor))
+//     localStorage.setItem('total', valorTotal);
+//     localStorage.setItem('valorMercado', valorMercado);
+//     localStorage.setItem('valorTransporte', valorTransporte);
+//     window.location.href = 'resumo.html';
+// }
 
-
-const extrato = document.querySelector('.extrato');
-const categoria = document.getElementById('categoria');
-const valor = document.getElementById('valor');
-const data = document.getElementById('data');
-const valorErro = document.getElementById('valor-erro');
-var valorMercado = 0;
-var valorTransporte = 0;
 
 // const extrato = document.querySelector('.extrato');
 // const categoria = document.getElementById('categoria');
 // const valor = document.getElementById('valor');
 // const data = document.getElementById('data');
 // const valorErro = document.getElementById('valor-erro');
+// var valorMercado = 0;
+// var valorTransporte = 0;
 
-// let valorMercado = 0;
-// let valorTransporte = 0;
-// let valorTotal = 0;
-// let mesesValor = {};
+const extrato = document.querySelector('.extrato');
+const categoria = document.getElementById('categoria');
+const valor = document.getElementById('valor');
+const data = document.getElementById('data');
+const valorErro = document.getElementById('valor-erro');
 
-// function adicionar() {
-//     const novaLinha = extrato.insertRow(-1);
-//     const celulaCategoria = novaLinha.insertCell(0);
-//     const celulaData = novaLinha.insertCell(1);
-//     const celulaValor = novaLinha.insertCell(2);
+let valorMercado = 0;
+let valorTransporte = 0;
+let valorTotal = 0;
+let mesesValor = new Object();
+let mesesAnos = [];
+
+function adicionar() {
+    let c = 0;
+    const novaLinha = extrato.insertRow(-1);
+    const celulaCategoria = novaLinha.insertCell(0);
+    const celulaData = novaLinha.insertCell(1);
+    const celulaValor = novaLinha.insertCell(2);
   
-//     let categoriaNova = '';
-//     switch (categoria.value) {
-//       case 'mercado':
-//         categoriaNova = 'Mercado';
-//         break;
-//       case 'transporte':
-//         categoriaNova = 'Transporte';
-//         break;
-//     }
+    let categoriaNova = '';
+    switch (categoria.value) {
+      case 'mercado':
+        categoriaNova = 'Mercado';
+        break;
+      case 'transporte':
+        categoriaNova = 'Transporte';
+        break;
+    }
   
-//     celulaCategoria.appendChild(document.createTextNode(categoriaNova));
-//     celulaValor.appendChild(document.createTextNode(parseInt(valor.value)));
+    celulaCategoria.appendChild(document.createTextNode(categoriaNova));
+    celulaValor.appendChild(document.createTextNode(parseFloat(valor.value)));
   
-//     if (getCategoriaSelecionada() === 'transporte') {
-//       valorTransporte += parseInt(valor.value);
-//     } else if (getCategoriaSelecionada() === 'mercado') {
-//       valorMercado += parseInt(valor.value);
-//     }
+    if (getCategoriaSelecionada() === 'transporte') {
+      valorTransporte += parseFloat(valor.value);
+    } else if (getCategoriaSelecionada() === 'mercado') {
+      valorMercado += parseFloat(valor.value);
+    }
   
-//     const dataNova = new Date(data.value);
-//     // const novaData = dataNova.toLocaleDateString('pt-BR');
-//     celulaData.appendChild(document.createTextNode(dataNova));
+    const dataNova = new Date(data.value);
+    const dataFormatada = new Date(Date.parse(dataNova));
+    dataFormatada.setDate(dataFormatada.getDate() + 1); 
+    let dataFormatadaTratada = dataFormatada.toLocaleDateString('pt-BR');
+
+    celulaData.appendChild(document.createTextNode(dataFormatadaTratada));
+
     
-//     const mes = dataNova.getMonth() + 1;
-//     const ano = dataNova.getFullYear();
-//     const chave = `${ano}${mes.toString().padStart(2, '0')}`;
+    const mes = dataFormatada.getMonth() + 1;
+    const ano = dataFormatada.getFullYear();
+    const chave = `${ano}/${mes}`;
     
-//     if (mesesValor[chave]) {
-//       mesesValor[chave] += parseInt(valor.value);
-//     } else {
-//       mesesValor[chave] = parseInt(valor.value);
-//     }
-  
-//     let linhas = extrato.getElementsByTagName('tr');
-//     let posicao = 1;
-//     while (posicao < linhas.length && new Date(linhas[posicao].getElementsByTagName('td')[1].innerHTML) < dataNova) {
-//       posicao++;
-//     }
-  
-//     if (posicao < linhas.length) {
-//       extrato.insertBefore(novaLinha, linhas[posicao]);
-//     } else {
-//       extrato.appendChild(novaLinha);
-//     }
-  
-//     categoria.value = '';
-//     valor.value = '';
-//     data.value = '';
-//     verificarValor();
-// }
+    if (mesesValor[chave]) {
+      mesesValor[chave] += parseFloat(valor.value);
+      mesesAnos.push([ano, mes])
+    } else {
+      mesesValor[chave] = parseFloat(valor.value);
+      mesesAnos.push([ano, mes])
+    }
+
+    categoria.value = '';
+    valor.value = '';
+    data.value = '';
+}
   
 
-// function calcular() {
-//   const linhas = extrato.getElementsByTagName('tr');
+function calcular() {
+  const linhas = extrato.getElementsByTagName('tr');
 
-//   for (let i = 1; i < linhas.length; i++) {
-//     const valorCelulaValor = parseFloat(linhas[i].getElementsByTagName('td')[2].innerHTML);
-//     valorTotal += valorCelulaValor;
-//   }
+  for (let i = 1; i < linhas.length; i++) {
+    const valorCelulaValor = parseFloat(linhas[i].getElementsByTagName('td')[2].innerHTML);
+    valorTotal += valorCelulaValor;
+  }
 
-//   localStorage.setItem('mesesValorObject', mesesValor);
-//   localStorage.setItem('mesesValor', JSON.stringify(mesesValor));
-//   localStorage.setItem('total', valorTotal);
-//   localStorage.setItem('valorMercado', valorMercado);
-//   localStorage.setItem('valorTransporte', valorTransporte);
-//   window.location.href = 'resumo.html';
-// }
+  localStorage.setItem('mesesAnos', JSON.stringify(mesesAnos));
+  localStorage.setItem('mesesValor', JSON.stringify(mesesValor));
+  localStorage.setItem('total', valorTotal);
+  localStorage.setItem('valorMercado', valorMercado);
+  localStorage.setItem('valorTransporte', valorTransporte);
+  window.location.href = 'resumo.html';
+}
 
-// function verificarValor() {
-//   valorErro.innerHTML = '';
+function getCategoriaSelecionada() {
+  return categoria.options[categoria.selectedIndex].value;
+}
 
-//   if (parseInt(valor.value) < 0) {
-//     valorErro.style.color = 'red';
-//     valorErro.innerHTML = 'Valor deve ser maior ou igual a 0';
-//   }
-// }
+function verificarValor() {
+    valorErro.innerHTML = '';
+    let valorConvertido = parseFloat(valor.value);
+    if (isNaN(valorConvertido) || valorConvertido < 0) {
+        valorErro.style.color = 'red';
+        valorErro.innerHTML = 'Valor deve ser maior ou igual a 0';
+    }
+}
 
-// function getCategoriaSelecionada() {
-//   return categoria.options[categoria.selectedIndex].value;
-// }
--- ex12/js/resumo.js
+++ ex12/js/resumo.js
@@ -1,50 +1,28 @@
 window.onload = () => {
-
-    
-    let anos = localStorage.getItem('arrayAnos')
     let total = localStorage.getItem('total');
-    let mesesValorString = localStorage.getItem('mesesValor')
+    let mesesAnosString = localStorage.getItem('mesesAnos');
+    let mesesValorString = localStorage.getItem('mesesValor');
+    let mesesValor = JSON.parse(mesesValorString);
+    let mesesAnos = JSON.parse(mesesAnosString);
     let valorMercado = localStorage.getItem('valorMercado');
     let valorTransporte = localStorage.getItem('valorTransporte');
-    let tabela = document.querySelector('.tabela-resultado')
-    let mesesValor = JSON.parse(mesesValorString);
-    console.log(anos)
+    let tabela = document.querySelector('.tabela-resultado');
+    let c = 0;
+    let chavesContidas = []
 
+    console.log(mesesValor)
+    console.log(mesesAnos)
     let resultadoTotal = document.getElementById('resultado-total');
     let resultadoTotalValor = document.getElementById('resultado-total-valor');
 
     let resultadoMercado = document.getElementById('resultado-mercado');
     let resultadoMercadoValor = document.getElementById('resultado-mercado-valor');
-    
+
     let resultadoTransporte = document.getElementById('resultado-transporte')
     let resultadoTransporteValor = document.getElementById('resultado-transporte-valor');
-    
-    resultadoTotal.innerHTML = `Valor total`;
-    resultadoTotalValor.innerHTML = `R$ ` + parseFloat(total).toFixed(2);
 
-    for(let mes in mesesValor){
-        let novaLinha = tabela.insertRow(-1);
-        let celulaMes = novaLinha.insertCell(0);
-        let celulaValor = novaLinha.insertCell(1);
-        let valor = mesesValor[mes];
-        let mesNome = ''
-        switch(mes){
-            case '1':
-                mesNome = ' de Janeiro'
-                break;
-            case '2':
-                mesNome = ' de Fevereiro'
-                break;
-            case '3':
-                mesNome = ' de Março'
-                break;
-            case '4':
-                mesNome = ' de Abril'
-                break;
-        }
-        celulaMes.textContent = `Total no mês de ${mesNome} de `;
-        celulaValor.textContent = `R$${parseFloat(valor).toFixed(2)}`;
-    }
+    resultadoTotal.innerHTML = `Valor total`;
+    resultadoTotalValor.innerHTML = `R$` + parseFloat(total).toFixed(2);
 
     resultadoMercado.innerHTML = `Total de gastos com mercado`
     resultadoMercadoValor.innerHTML = `R$` + parseFloat(valorMercado).toFixed(2);
@@ -53,9 +31,65 @@ window.onload = () => {
     resultadoTransporteValor.innerHTML = `R$` + parseFloat(valorTransporte).toFixed(2);
 
 
-
+    for(chave in mesesValor){
+        for (const mesAno of mesesAnos){
+                if(!chavesContidas.includes(chave)){
+                    let novaLinha = tabela.insertRow(-1);
+                    let celulaMesAno = novaLinha.insertCell(0);
+                    var celulaValor = novaLinha.insertCell(1);
+                    let mesNome;
+                    switch (mesesAnos[c][1]) {
+                        case 1:
+                            mesNome = ' Janeiro'
+                            break;
+                        case 2:
+                            mesNome = ' Fevereiro'
+                            break;
+                        case 3:
+                            mesNome = ' Março'
+                            break;
+                        case 4:
+                            mesNome = ' Abril'
+                            break;
+                        case 5:
+                            mesNome = ' Maio'
+                            break;
+                        case 6:
+                            mesNome = ' Junho'
+                            break;
+                        case 7:
+                            mesNome = ' Julho'
+                            break;
+                        case 8:
+                            mesNome = ' Agosto'
+                            break;
+                        case 9:
+                            mesNome = ' Setembro'
+                            break;
+                        case 10:
+                            mesNome = ' Outubro'
+                            break;
+                        case 11:
+                            mesNome = ' Novembro'
+                            break;
+                        case 12:
+                            mesNome = ' Dezembro'
+                            break; 
+                    
+                    }
+                    celulaMesAno.textContent = `Total no mês de ${mesNome} de ${mesesAnos[c][0]}`;
+                    let valor = mesesValor[chave];
+                    celulaValor.textContent = `R$${parseFloat(valor).toFixed(2)}`;
+                    chavesContidas.push(chave)
+                    c += 1; 
+                }
+            
+        }
+    }
     localStorage.removeItem('total');
     localStorage.removeItem('mesesValor');
     localStorage.removeItem('valorMercado');
     localStorage.removeItem('valorTransporte');
 }
+
+
